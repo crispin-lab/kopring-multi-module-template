cff746de253162593e7cdd90553e28a7da3cbc77
crispin <h.c.shin.dev09@gmail.com>
2025-02-03 22:07:44 +0900

    :sparkles: Feat: build gradle 설정 및 build-logic 모듈 추가

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,17 @@
 rootProject.name = "kopring-multi-module-template"
+
+pluginManagement {
+    includeBuild("build-logic")
+    repositories {
+        mavenCentral()
+        gradlePluginPortal()
+    }
+
+    dependencyResolutionManagement {
+        repositories {
+            mavenCentral()
+        }
+    }
+}
+
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
